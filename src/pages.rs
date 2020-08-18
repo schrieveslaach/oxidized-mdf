e8bf8284cca9ae9bfdc7fadcdb21450d8fbd3fff
Marc Schreiber <info@schrieveslaach.de>
2020-08-18 21:25:27 +0200

    Parse page header

--- src/pages.rs
+++ src/pages.rs
@@ -2,7 +2,11 @@ use byteorder::{BigEndian, ReadBytesExt};
 use std::convert::TryFrom;
 use std::iter::FromIterator;
 
+pub(crate) struct PageHeader {
+    pub(crate) slot_count: i16,
+}
 pub(crate) struct BootPage {
+    pub(crate) header: PageHeader,
     pub(crate) database_name: String,
     pub(crate) first_sys_indexes: PagePointer,
 }
@@ -27,6 +31,8 @@ impl TryFrom<[u8; 8192]> for BootPage {
     type Error = &'static str;
 
     fn try_from(bytes: [u8; 8192]) -> Result<Self, Self::Error> {
+        let header = PageHeader::try_from(&bytes[0..96])?;
+
         let (s, _, _) = encoding_rs::UTF_16LE.decode(&bytes[148..(404)]);
         let database_name = String::from_iter(s.chars().filter(|c| *c != '†'));
 
@@ -35,6 +41,35 @@ impl TryFrom<[u8; 8192]> for BootPage {
             file_id: (&bytes[616..618]).read_u16::<BigEndian>().unwrap(),
         };
 
-        Ok(Self { database_name, first_sys_indexes })
+        Ok(Self {
+            header,
+            database_name,
+            first_sys_indexes,
+        })
+    }
+}
+
+/// Converts the given bytes into a `PageHeader`.
+///
+/// ```text
+/// Bytes       Content
+/// -----		-------
+/// ...         ?
+/// 22-23       SlotCnt (smallint)
+/// ...         ?
+/// ```
+impl TryFrom<&[u8]> for PageHeader {
+    type Error = &'static str;
+
+    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
+        assert!(
+            bytes.len() == 96,
+            "Page header must be 96 bytes but was {}.",
+            bytes.len()
+        );
+
+        Ok(PageHeader {
+            slot_count: (&bytes[22..24]).read_i16::<BigEndian>().unwrap(),
+        })
     }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,16 @@
 use async_std::io::Result;
 use oxidized_mdf::MdfDatabase;
+use rstest::rstest;
 
+#[rstest(
+    file,
+    db_name,
+    case("spg_verein_TST.mdf", "spg_verein_TST"),
+    case("AWLT2005.mdf", "AdventureWorksLT")
+)]
 #[async_std::test]
-async fn test_read_spg_verein_sample_database_name() -> Result<()> {
-    let db = MdfDatabase::open("tests/spg_verein_TST.mdf").await?;
-    assert_eq!(db.database_name(), "spg_verein_TST");
+async fn test_read_spg_verein_sample_database_name(file: &str, db_name: &str) -> Result<()> {
+    let db = MdfDatabase::open(format!("tests/{}", file)).await?;
+    assert_eq!(db.database_name(), db_name);
     Ok(())
 }
